6b5765ea5857a6541477150b2777e43748eec062
Jacob Paltrowitz <jacobpaltrow@gmail.com>
2023-06-30 15:26:22 -0400

    It worksgit add sdrtest.py

--- sdrtest.py
+++ sdrtest.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
+from pylab import *
 import numpy as np
 from numpy.fft import fft, ifft
-from rtlsdr import *
+from rtlsdr import * #There were some issues using this on Windows, may need to use WSL or something?
 import math
 
 sdr = RtlSdr()
@@ -9,41 +9,31 @@ sdr = RtlSdr()
 # configure device
 sdr.sample_rate = 2.4e6
 sample_rate = sdr.sample_rate
-sdr.center_freq = 4.22e6 #should be set to desired frequency
+sdr.center_freq = 4e6 #should be set to desired frequency
 center_freq = sdr.center_freq
-sdr.gain = 4
+sdr.gain = 40.2
+sdr.set_direct_sampling(2)
 Time = 10.0 # ms (can be decimal)
 
 #CHANGE NUMBER OF SAMPLES USING TIME
-NumberOfSamples = 256*math.floor(sample_rate*Time/(256*1000)) #number of samples must be a multiple of 256
+# NumberOfSamples = 256*math.floor(sample_rate*Time/(256*1000)) #number of samples must be a multiple of 256
+NumberOfSamples = 1024*256
 samples = sdr.read_samples(NumberOfSamples) #read in data
 sdr.close()
 
 #Plot data
 #Time domain first
 time_axis = np.linspace(0, NumberOfSamples/sample_rate, int(NumberOfSamples))
-plt.plot(time_axis/1e-3, samples.real, 'g')
-plt.plot(time_axis/1e-3, samples.imag, 'r')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
+plot(time_axis/1e-3, samples.real, 'g')
+plot(time_axis/1e-3, samples.imag, 'r')
+xlabel('Time (ms)')
+ylabel('Amplitude')
 
-plt.show()
+show()
 
-#FFT to have Frequency Domain Second
-X = fft(samples)
-N = len(X)
-n = np.arange(N)
-T = N/sample_rate
-freq = n/T
+# FFT to have Frequency Domain Second
+psd(samples, NFFT=1024, Fs=sample_rate/1e6, Fc=center_freq/1e6)
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
 
-plt.figure(figsize = (12, 6))
-plt.subplot(121)
-
-plt.stem(freq, np.abs(X), 'b', \
-         markerfmt=" ", basefmt="-b")
-plt.xlabel('Freq (Hz)')
-plt.ylabel('FFT Amplitude |X(freq)|')
-
-plt.show()
-
-# plt.show()
+show()
